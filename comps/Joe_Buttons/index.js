584dbe9c476eed4cabd83b67c66e3185c98daf59
nguyenlejoe <joe_le.1@hotmail.com>
2020-04-09 18:03:42 -0700

    Added button story and made a temporary button

---
+++ comps/Joe_Buttons/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './button.css';
+
+const CustomButton = ({text, onclick,color}) =>  <div className = "customButtonBox">
+    <div className = "customButton">
+     {text}
+    </div>
+</div>
+
+CustomButton.defaultProps = {
+    text: "Click here",
+    color: "#F7FAFD",
+
+}
+
+
+export default CustomButton;
