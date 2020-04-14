fd8091541398e9d2b94929fad24e176da1ce801f
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-04-13 23:18:07 -0700

    made/style body text with variables & defaultProps

---
+++ stories/BodyText.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import BodyText from "../comps/Noah_BodyText";
+
+export default {
+  title: "BodyText",
+  component: BodyText,
+};
+
+export const customBodyText = () => <BodyText />;
