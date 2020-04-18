681532745a12ed844ce3a58851cac9f50f5c73c4
= <=>
2020-04-18 12:42:59 -0700

    made the slider look a little better

---
+++ .history/comps/Alex_Sliders/index_20200418124131.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Alex_Sliders/slider.css";
+
+const Slider = () => {
+    <div>
+        <div class="border">
+            <input type="range" min="1" max="3" value="1" class="slider" id="myRange" />
+            <p>Value: <span id="valueSlider"></span></p>
+        </div>
+    </div>
+
+
+    var slider = document.querySelector("#myRange");
+    var output = document.querySelector("#value");
+
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+    }
+
+    slider.addEventListener("mousemove", function () {
+        var x = slide.value;
+        var color = "linear-gradient(90deg, rgb(0, 163, 255)' + x + '%, rgb(13, 230, 245, 0.103) + x + '%')"
+        slider.style.background = color;
+    })
+}
+export default Slider;
