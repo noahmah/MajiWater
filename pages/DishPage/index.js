13516c8484775c498fc8aac6da46eb64a917aacb
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-04-21 15:53:13 -0700

    pushing again

--- pages/DishPage/index.js
+++ pages/DishPage/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import './dish_page.css';
-import CustomHeader from '../../comps/Joe_Header';
-import BodyText from '../../comps/Noah_BodyText';
-import ImgFrame from '../../comps/Alex_Images';
-import CustomButton from '../../comps/Joe_Buttons';
-import ProgBar from '../../comps/Noah_ProgressBar';
+import React from "react";
+import "./dish_page.css";
+import CustomHeader from "../../comps/Joe_Header";
+import BodyText from "../../comps/Noah_BodyText";
+import ImgFrame from "../../comps/Alex_Images";
+import CustomButton from "../../comps/Joe_Buttons";
+import ProgBar from "../../comps/Noah_ProgressBar";
 
-
-
-const DishPage = ({ }) => {
-
-    return <div>
+const DishPage = ({}) => {
+  return (
+    <div>
+      <div id="header">
         <CustomHeader />
-        <BodyText text='Did you use the dishwasher today?' />
-        <ImgFrame img={'https://images.unsplash.com/photo-1581622558663-b2e33377dfb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3334&q=80'} />
-        < CustomButton text='Yes' />
-        <CustomButton text='No' />
-        <ProgBar />
+      </div>
+      <div id="hero-image">
+        <BodyText text="Did you use the dishwasher today?" />
+      </div>
+      <div id="hero-image">
+        <ImgFrame
+          img={
+            "https://images.unsplash.com/photo-1581622558663-b2e33377dfb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3334&q=80"
+          }
+        />
+      </div>
+      <div id="btns">
+        <div id="btns-cont">
+          <div id="btns-box">
+            <div id="btn-margin">
+              <CustomButton color="#F7FAFD" text="Yes" />
+            </div>
+            <CustomButton color="#F7FAFD" text="No" />
+          </div>
+        </div>
+      </div>
+      div id="prog-box">
+      <div id="prog-cont">
+        <div id="prog-bar">
+          <ProgBar />
+        </div>
+      </div>
     </div>
-}
-
+  );
+};
 
 export default DishPage;
--- pages/ResultsPage/index.js
+++ pages/ResultsPage/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './result_page.css';
-import CustomHeader from '../../comps/Joe_Header';
-import BodyText from '../../comps/Noah_BodyText';
-import ImgFrame from '../../comps/Alex_Images';
-import CustomButton from '../../comps/Joe_Buttons';
-import ProgBar from '../../comps/Noah_ProgressBar';
+import React from "react";
+import "./result_page.css";
+import CustomHeader from "../../comps/Joe_Header";
+import BodyText from "../../comps/Noah_BodyText";
+import ImgFrame from "../../comps/Alex_Images";
+import CustomButton from "../../comps/Joe_Buttons";
+import ProgBar from "../../comps/Noah_ProgressBar";
 
-const ResultPage = () =>
-    <div>
-        <CustomHeader />
-        <BodyText text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' />
-        <BodyText text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' />
-        <ImgFrame img={'https://images.unsplash.com/photo-1497655392221-e645087843da?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80'} />
-        <ProgBar />
-
-    </div>
-
-
-
-
+const ResultPage = () => (
+  <div>
+    <CustomHeader />
+    <BodyText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" />
+    <BodyText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+    <ImgFrame
+      img={
+        "https://images.unsplash.com/photo-1497655392221-e645087843da?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
+      }
+    />
+    <ProgBar />
+  </div>
+);
 
 export default ResultPage;
