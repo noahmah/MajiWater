e59b54083e2535d1f88f65e19cde1414d172eb16
nguyenlejoe <joe_le.1@hotmail.com>
2020-05-10 00:26:25 -0700

    Did some testing

--- comps/Noah_ProgressBar/index.js
+++ comps/Noah_ProgressBar/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./progressBar.css";
 
-const ProgBar = ({ background }) => (
+const ProgBar = ({ color}) => (
   <div>
+ 
+    {/* <div className="progBar" style={{ backgroundColor: background }}></div> */}
     <div className="progBarCont">
+    <div className="progBar" style={{backgroundColor:color}}></div>
       <div className="lines">
         <div id="line"></div>
         <div id="line"></div>
         <div id="line"></div>
       </div>
-      <div className="progBar" style={{ background: background }}></div>
+      {/* <div className="progBar" style={{backgroundColor:BacColor}}></div> */}
     </div>
   </div>
 );
 
-ProgBar.defaultprops = {
-  background: "linear-gradient(to right, #00ffe2 0%, #0079ff 100%);",
+ProgBar.defaultProps = {
+  color: "blue"
+  // color: "linear-gradient(to right, #00ffe2 0%, #0079ff 100%);",
 };
 
 export default ProgBar;
