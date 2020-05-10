81ac0d9c5b181ef18d279bb6ad2897a2582e9a2d
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-05-10 00:05:28 -0700

    set up props for progbar

--- comps/Noah_ProgressBar/index.js
+++ comps/Noah_ProgressBar/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./progressBar.css";
 
-const ProgBar = () =>
- <div>
+const ProgBar = ({ background }) => (
+  <div>
     <div className="progBarCont">
       <div className="lines">
         <div id="line"></div>
         <div id="line"></div>
         <div id="line"></div>
       </div>
-      <div className="progBar"></div>
+      <div className="progBar" style={{ background: background }}></div>
     </div>
   </div>
+);
 
+ProgBar.defaultprops = {
+  background: "linear-gradient(to right, #00ffe2 0%, #0079ff 100%);",
+};
 
 export default ProgBar;
