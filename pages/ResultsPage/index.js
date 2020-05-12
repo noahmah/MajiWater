15d4c75eafd71532301ef0bd56b02aff8b9b6cde
nguyenlejoe <joe_le.1@hotmail.com>
2020-05-11 23:42:48 -0700

    New result page style

--- pages/ResultsPage/index.js
+++ pages/ResultsPage/index.js
@@ -9,6 +9,7 @@ import CustomButton from "../../comps/Joe_Buttons";
 import ProgBar from "../../comps/Noah_ProgressBar";
 import { data, ChangeData, checkPath } from "../../data";
 import CustomMenu from "../../comps/Noah_Menu";
+import ResultBox from "../../comps/Joe_ResultBox";
 import { useEffect } from "react";
 
 console.log(data);
@@ -38,55 +39,73 @@ const ResultPage = () => {
         <CustomHeader text="Results" />
       </div>
       <div id="results-text">
-        <BodyText fontSize="18px" text={data.results[0].question} />
-        <BodyText color="#000000" text={data.results[0].answer} />
+
+        <BodyText
+        text="What you chose"
+        fontSize="22px"
+        fontWeight="Bold"
+        />
+
+        <div className="UserAnswer">
+        <BodyText fontSize="16px"  text={data.results[0].question} />
+        <BodyText color="#000000" fontWeight="bold" text={data.results[0].answer} />
+        </div>
+
+        <div className="UserAnswer">
         {data.results[0].answer === "Shower" ? (
-          <BodyText fontSize="18px" text={data.results[1].question} />
+          <BodyText fontSize="16px" text={data.results[1].question} />
+        ) : null}
+        {data.results[0].answer === "Shower" ? (
+          <BodyText color="#000000" fontWeight="bold"text={data.results[1].answer} />
+        ) : null}
+        </div>
+
+        <div className="UserAnswer">
+        {data.results[0].answer === "Bath" ? (
+          <BodyText fontSize="16px" text={data.results[2].question} />
         ) : null}
         {data.results[0].answer === "Bath" ? (
-          <BodyText fontSize="18px" text={data.results[2].question} />
+          <BodyText color="#000000" fontWeight="bold" text={data.results[2].answer} />
         ) : null}
-        {data.results[0].answer === "Shower" ? (
-          <BodyText color="#000000" text={data.results[1].answer} />
-        ) : null}
-        {data.results[0].answer === "Bath" ? (
-          <BodyText color="#000000" text={data.results[2].answer} />
-        ) : null}
-        <BodyText fontSize="18px" text={data.results[3].question} />
-        <BodyText color="#000000" text={data.results[3].answer} />
+        </div>
+
+        <div className="UserAnswer">
+        <BodyText fontSize="16px" text={data.results[3].question} />
+        <BodyText color="#000000" fontWeight="bold" text={data.results[3].answer} />
+        </div>
+
+        <div className="UserAnswer">
         {data.results[3].answer === "Yes" ? (
-          <BodyText fontSize="18px" text={data.results[4].question} />
+          <BodyText fontSize="16px" text={data.results[4].question} />
         ) : null}
         {data.results[3].answer === "Yes" ? (
-          <BodyText color="#000000" text={data.results[4].answer} />
+          <BodyText color="#000000" fontWeight="bold" text={data.results[4].answer} />
         ) : null}
-        <BodyText fontSize="18px" text={data.results[5].question} />
-        <BodyText color="#000000" text={data.results[5].answer} />
+        </div>
+
+        <div className="UserAnswer">
+        <BodyText fontSize="16px" text={data.results[5].question} />
+        <BodyText color="#000000" fontWeight="bold" text={data.results[5].answer} />
+        </div>
 
         <div id="horizontal-line"></div>
 
-        <BodyText text={data.waterUsage} />
-        <BodyText text={data.fact} />
-        {data.resultPath.suggestion1 != "" ? (
-          <BodyText text={data.resultPath.suggestion1} />
-        ) : null}
-        {data.resultPath.suggestion2 != "" ? (
-          <BodyText text={data.resultPath.suggestion2} />
-        ) : null}
-        {data.resultPath.suggestion3 != "" ? (
-          <BodyText text={data.resultPath.suggestion3} />
-        ) : null}
-      </div>
-      <div id="hero-image">
-        <ImgFrame
-          img={
-            "https://images.unsplash.com/photo-1497655392221-e645087843da?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
-          }
+        <BodyText
+        text="Results"
+        fontSize="22px"
+        fontWeight="Bold"
         />
+
+          <ResultBox
+            amountCon={data.waterUsage}
+            facts={data.fact}
+            suggestion1={data.resultPath.suggestion1}
+            suggestion2={data.resultPath.suggestion2}
+            suggestion3={data.resultPath.suggestion3}
+            />
+      
       </div>
-      <div id="prog-box">
-        <ProgBar color="linear-gradient(to right, #00ffe2 100%, #0079ff 0%)" />
-      </div>
+        
     </div>
   );
 };
