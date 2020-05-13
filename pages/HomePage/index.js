2a6ab46f647826067681765baa170df4faca480b
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-05-12 21:11:55 -0700

    changed prog bar colors

--- pages/HomePage/index.js
+++ pages/HomePage/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './home_page.css';
+import React from "react";
+import "./home_page.css";
 
-import CustomHeader from '../../comps/Joe_Header';
-import CustomButton from '../../comps/Joe_Buttons';
-import ProgBar from '../../comps/Noah_ProgressBar';
+import CustomHeader from "../../comps/Joe_Header";
+import CustomButton from "../../comps/Joe_Buttons";
+import ProgBar from "../../comps/Noah_ProgressBar";
 
-
-const HomePage = () =>
-    <div>
-        <CustomHeader />
-        <CustomButton text='Start' />
-        <CustomButton text='About' />
-        <ProgBar />
-    </div>
-
+const HomePage = () => (
+  <div>
+    <CustomHeader />
+    {/* <CustomHeader /> */}
+    <CustomButton text="Start" />
+    <CustomButton text="About" />
+    <ProgBar />
+  </div>
+);
 
 export default HomePage;
